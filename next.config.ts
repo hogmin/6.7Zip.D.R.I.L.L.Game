9c05446735c41a90c644b829259aa15b7d49821c
ZhenyaLaptev <vinditop909@gmail.com>
2026-03-14 10:09:48 +0200

    Remove basePath for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/6.7Zip.D.R.I.L.L.Game',
   images: {
     unoptimized: true,
   },
